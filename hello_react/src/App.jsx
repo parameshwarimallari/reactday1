8ce91217ebcf1e13dc57242c8345208533dea1cc
parameshwarimallari <parameshwarimallari5@gmail.com>
2025-10-24 19:17:07 +0530

    react day1 task

---
+++ hello_react/src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+// import Hello,{Second} from './day1'
+
+function App() {
+
+  let s="parmi"
+  let language="reactjs"
+  
+  return (<div><h1>{s}</h1><h1>{language}</h1></div>)
+}
+export default App
---
+++ hello_react/src/day1.jsx
@@ -0,0 +1,7 @@
+ function Hello(){
+    return <h1>first</h1>
+}
+export function Second(){
+    return <h1>second</h1>
+}
+ export default Hello;
